fc9b191bc27ee94c295df652a3b2b558b71342eb
Aiwe <maksymivv@gmail.com>
2020-05-25 20:43:34 +0300

    Deduced return type only available with -std=c++14 or -std=gnu++14, fix it

--- src/crypto/random.h
+++ src/crypto/random.h
@@ -93,7 +93,7 @@ namespace Random
      * Obtain the generator used internally. Helpful for passing to functions
      * like std::shuffle.
      */
-    inline auto generator()
+    inline std::mt19937 generator()
     {
         return gen;
     }
