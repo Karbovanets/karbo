afbb63479d03dc5c2d70f658e1c0c831067687e5
Aiwe <maksymivv@gmail.com>
2020-12-11 17:17:13 +0200

    Just rename local peerlist

--- src/P2p/NetNode.cpp
+++ src/P2p/NetNode.cpp
@@ -1382,11 +1382,11 @@ std::string print_banlist_to_string(std::map<uint32_t, time_t> list) {
     //fill response
     rsp.local_time = time(nullptr);
 
-    std::vector<PeerlistEntry> local_peerlist_new;
-    m_peerlist.get_peerlist_head(local_peerlist_new);
+    std::vector<PeerlistEntry> local_peerlist;
+    m_peerlist.get_peerlist_head(local_peerlist);
     //only include out peers we did not already send
-    rsp.local_peerlist.reserve(local_peerlist_new.size());
-    for (auto &pe : local_peerlist_new)
+    rsp.local_peerlist.reserve(local_peerlist.size());
+    for (auto &pe : local_peerlist)
     {
       if (!context.sent_addresses.insert(pe.adr).second)
         continue;
