ea0208a372129023006261615b532327250d5ae7
Aiwe <maksymivv@gmail.com>
2021-07-08 11:08:55 +0300

    It's 2021

--- src/CryptoNoteCore/Miner.cpp
+++ src/CryptoNoteCore/Miner.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018-2019, The TurtleCoin Developers
-// Copyright (c) 2016-2020, The Karbo developers
+// Copyright (c) 2016-2021, The Karbo developers
 //
 // This file is part of Karbo.
 //
