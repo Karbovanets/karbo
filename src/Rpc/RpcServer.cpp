147c1767db27eb85e6a3e5d4168e391b006c452e
aiwe <maksymivv@gmail.com>
2024-04-28 12:35:20 -0500

    Enhance nor rpc server index output, add emission and next reward info

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -692,7 +692,7 @@ bool RpcServer::onGetPoolChangesLite(const COMMAND_RPC_GET_POOL_CHANGES_LITE::re
 
 bool RpcServer::onGetIndex(const COMMAND_HTTP::request& req, COMMAND_HTTP::response& res) {
   const std::string index_start =
-    R"(<html><head><meta http-equiv='refresh' content='60'/></head><body><p><svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" version="1.1" style="vertical-align:middle; padding-right: 10px; shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 2500000 2500000" xmlns:xlink="http://www.w3.org/1999/xlink" width="64px" height="64px">
+    R"(<html><head><meta http-equiv='refresh' content='60'/><style>* { font-family: monospace;</style></head><body><p><svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" version="1.1" style="vertical-align:middle; padding-right: 10px; shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 2500000 2500000" xmlns:xlink="http://www.w3.org/1999/xlink" width="64px" height="64px">
 <g>
 <circle fill="#0AACFC" cx="1250000" cy="1250000" r="1214062" />
 <path fill="#FFED00" d="M1251219 1162750c18009,-3203 34019,-10006 48025,-20412 14009,-10407 27215,-28016 39622,-52029l275750 -538290c10803,-18010 24012,-32419 39218,-43625 15210,-10806 33219,-16410 53232,-16410l174893 0 -343384 633144c-15209,26016 -32419,47228 -51628,63635 -19613,16409 -41225,28815 -64838,37221 36822,9604 67638,25213 92854,47225 24812,21610 48425,52025 70437,91247l330578 668363 -192503 0c-38822,0 -70041,-21213 -93653,-63235l-270947 -566303c-14006,-25215 -29216,-43225 -45622,-54034 -16409,-10803 -37222,-17206 -62034,-18809l0 287359 -151281 0 0 -288559 -111263 0 0 703581 -213716 0 0 -1540835 213716 0 0 673166 111263 0 0 -332981 151281 0 0 330581z"/>
@@ -712,6 +712,9 @@ bool RpcServer::onGetIndex(const COMMAND_HTTP::request& req, COMMAND_HTTP::respo
   size_t alt_blocks_count = m_core.getAlternativeBlocksCount();
   size_t total_tx_count = m_core.getBlockchainTransactionsCount() - top_block_index + 1;
   size_t tx_pool_count = m_core.getPoolTransactionsCount();
+  uint64_t alreadyGeneratedCoins = m_core.getTotalGeneratedAmount();
+  std::string already_generated_coins = m_core.getCurrency().formatAmount(alreadyGeneratedCoins);
+  uint64_t next_reward = m_core.calculateReward(alreadyGeneratedCoins);
 
   const std::string body = index_start + PROJECT_VERSION_LONG + " &bull; " + (m_core.getCurrency().isTestnet() ? "testnet" : "mainnet") +
     "<ul>" +
@@ -721,6 +724,8 @@ bool RpcServer::onGetIndex(const COMMAND_HTTP::request& req, COMMAND_HTTP::respo
     "<li>" + "Alt. blocks: " + std::to_string(alt_blocks_count) + "</li>" +
     "<li>" + "Total transactions in network: " + std::to_string(total_tx_count) + "</li>" +
     "<li>" + "Transactions in pool: " + std::to_string(tx_pool_count) + "</li>" +
+    "<li>" + "Emission: " + already_generated_coins + "</li>" +
+    "<li>" + "Next reward: " + m_core.getCurrency().formatAmount(next_reward) + "</li>" +
     "<li>" + "Connections:" +
     "<ul>" +
     "<li>" + "RPC: " + std::to_string(getConnectionsCount()) + "</li>" +
